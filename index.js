2a988d8eb555d4ae1b5cf6e1246cf18b94107a2b
陈克锋 <sfuser@chenkefengdeMacBook-Pro.local>
2018-09-27 15:43:10 +0800

    fixbug

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 
-var program = require('commander');
+var program = require("commander");
 var download = require('download-git-repo');
 
-program.version("1.0.3", "-v, --version").command('init <name>').action(name => {
-  download('http://gitlab.sudaotech.com:sfuser/wx-mini-template#master', name, {clone: true}, error => {
-    console.log(error);
-  })
+program.version("1.0.4", "-v, --version").command('init <name>').action(name => {
+  // download('http://gitlab.sudaotech.com:sfuser/wx-mini-template#master', name, {clone: true}, error => {
+  //   console.log(error);
+  // })
+  console.log(name);
 });
 
 program.parse(process.argv);
