d3b752d634e5b10be7307d4c8ace66088a95f092
陈克锋 <sfuser@chenkefengdeMacBook-Pro.local>
2018-09-27 14:32:39 +0800

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const program = require('commander');
+const download = require('download-git-repo');
+
+program.version("1.0.0", "-v, --sersion").command('init <name>').action(name => {
+  console.log(name, "=======");
+  download('http://gitlab.sudaotech.com:sfuser/wx-mini-template#master', name, {clone: true}, error => {
+    console.log(error);
+  })
+});
+
+program.parse(process.argv);
