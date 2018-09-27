5f03f0be0dcd90a674ca2b6fbb78c32c14cee396
陈克锋 <sfuser@chenkefengdeMacBook-Pro.local>
2018-09-27 15:49:31 +0800

    fxibg

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 var program = require("commander");
 var download = require('download-git-repo');
 
-program.version("1.0.6", "-v, --version").command('init <name>').action(name => {
+program.version("1.0.7", "-v, --version").command('init <name>').action(name => {
   console.log(name);
-  download-git-repooad('http://gitlab.sudaotech.com:sfuser/wx-mini-template#master', name, {clone: true}, error => {
+  download('http://gitlab.sudaotech.com:sfuser/wx-mini-template#master', name, {clone: true}, error => {
     console.log(error);
   })
 
